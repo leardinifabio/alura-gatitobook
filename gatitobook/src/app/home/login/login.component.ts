ad21238501728c5b04a52a1baba6d375548f7ed1
Fabio Leardini <leardinifabio90@gmail.com>
2022-07-06 01:04:21 -0300

    validação de formulário e direcionamento de login

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      (data: any) => {
+        this.router.navigate(['animais']);
+      },
+      (error: any) => {
+        alert('Usuário ou senha inválidos');
+        console.log(error);
+      }
+    );
   }
-
 }
