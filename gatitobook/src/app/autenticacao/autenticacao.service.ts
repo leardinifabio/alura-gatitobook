7df8e45df848eb58044f5673852c04133db771de
Fabio Leardini <leardinifabio90@gmail.com>
2022-07-20 23:33:51 -0300

    instalacão jwt decode e iniciando configuracao de token

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,17 +1,51 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
+
+/*
+
+Entender melhor a função pipe():
+A função pipe() serve para ler dados de uma fonte assim que eles vão se tornando disponíveis e escrever esses dados em outro local.
+Função de efeito colateral.
+
+Por exemplo:
+É feito uma requisição HTTP e os dados vão chegando, conforme cada pedaço desses dados chegam, ele se torna disponível dentro da função pipe().
+
+pesquisar mais exemplos de pipe()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Entender melhor a função tap():
+A função tap() serve para fazer alguma ação com os dados que chegam.
+
+*/
